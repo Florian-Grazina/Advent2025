7c218b767b99549908068a2b585d8d4b7192ba63
Florian Grazina <fgrazina@ppi-group.lu>
2025-12-01 12:30:07 +0900

    Ajoutez des fichiers projet.

---
+++ Common/DayPuzzle.cs
@@ -0,0 +1,8 @@
+﻿namespace Common
+{
+    public interface DayPuzzle
+    {
+        int SolvePart1();
+        int SolvePart2();
+    }
+}
---
+++ Solver/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Common;
+
+List<DayPuzzle> puzzles =
+    [
+    null
+    ];
+
+foreach (DayPuzzle puzzle in puzzles)
+{
+    puzzle?.SolvePart1();
+    puzzle?.SolvePart2();
+
+}
+
+void Log(string message)
+{
+    Console.WriteLine(message);
+}
